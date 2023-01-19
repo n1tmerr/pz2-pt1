4389e4da73f367aa593755f4af59507267659ea3
is13131 <is13131@B206-1>
2023-01-19 09:25:33 +0700

    Добавьте файлы проекта.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static double first;
+        static double second;
+        static double hypotenuse;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите катет a");
+            first = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите катет b");
+            second = Convert.ToInt32(Console.ReadLine());
+            hypotenu();
+            
+        }
+
+            public static void hypotenu()
+            {
+            hypotenuse =  Math.Sqrt(Math.Pow(first, 2) + Math.Pow(second, 2));
+                Console.WriteLine($"Гипотенуза прямоугольного треугольника = {hypotenuse}");
+            }
+        }
+    }
+
